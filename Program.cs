cd27d15ae35ed3fee9f1934dc384403db1d3b9d4
Nataly Marcela Forero Villarraga <nforero@tfhkacol.local>
2026-08-03 14:43:39 -0500

    Primer Push con base de Proyecto Inventario y configuración de gitignore

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Console.WriteLine("Hello, World!");
+
+
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+
+Console.WriteLine("");
+Console.WriteLine("     SISTEMA DE GESTIÓN DE INVENTARIO       ");
+Console.WriteLine("");
+Console.WriteLine();
+
+Console.WriteLine($"Versión: {version}");
+
+// Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Platafroma: {Environment.OSVersion}");
+Console.WriteLine($".NET Versión: {Environment.Version}");
+// Console.WriteLine();
+// Console.WriteLine("Estado: Proyecto inicializao");
+
+Console.WriteLine();
+Console.WriteLine("Estructura del Proyecto");
+Console.WriteLine("Configuración .csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Proximo paso: Agregar argumentos CLI y configuración e Repositorio en GitHub");
